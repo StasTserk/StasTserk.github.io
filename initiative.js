92c2e2703d90893c39d19d6444de62212a5789e3
Stas <stas.tserkovny@gmail.com>
2016-05-01 16:20:18 -0600

    Fleshed out model binding
    Added more bindings
    Bootstrapped things up
    Fixed encoding issue

--- initiative.js
+++ initiative.js
@@ -2,9 +2,19 @@ var initiativeTracker = angular.module('initiativeTracker', []);
 
 initiativeTracker.controller('InitiativeController', function($scope) {
     $scope.participants = [
-        { 'name': 'Participant1', 'mod': 0, 'advantage': false, roll: false },
-        { 'name': 'Participant2', 'mod': 0, 'advantage': false, roll: false }
+        { 'name': 'Participant1', 'mod': 0, 'adv': true, 'roll': false },
+        { 'name': 'Participant2', 'mod': 0, 'adv': false, 'roll': true }
     ];
+
+    $scope.remove = function(participant) {
+        var index = $scope.participants.indexOf(participant);
+        $scope.participants.splice(index, 1);
+    }
+
+    $scope.addNew = function() {
+        $scope.participants.push(
+        { 'name': "Name", 'mod': 0, 'adv': false, 'roll': true });
+    }
 })
 
 var Tracker = {} || (
