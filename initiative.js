de1762f89bc655e23290799fe4c7354d2baffa6a
Stas <stas.tserkovny@gmail.com>
2016-05-02 19:10:11 -0600

    Added initiative rolling code
    Added code for rolling initiative for participants
    Now sorts combatants by initiative score

--- initiative.js
+++ initiative.js
@@ -6,13 +6,13 @@ initiativeTracker.controller('InitiativeController', function($scope) {
         { 'name': 'Participant2', 'mod': 0, 'adv': false, 'roll': true }
     ];
 
-    $scope.combatList = [
-        {
-            'name': 'Bonertown',
-            'score': 10,
-            'status': ''
-        }
-    ];
+    $scope.combatList = [];
+
+    $scope.rollInitiative = function() {
+        var newModel = Tracker.Initiative.Process($scope.participants);
+        $scope.combatList.length = 0;
+        $scope.combatList = $scope.combatList.concat(newModel);
+    }
 
     $scope.remove = function(participant) {
         var index = $scope.participants.indexOf(participant);
@@ -25,14 +25,50 @@ initiativeTracker.controller('InitiativeController', function($scope) {
     }
 })
 
-var Tracker = {} || (
-    function() {
-        function ProcessParticipantList(participants) {
-            
+var Tracker = Tracker || {};
+    Tracker.Initiative = (function() {
+
+        function RollInit(modifier, hasAdvantage)
+        {
+            var diceRoll = 0;
+            if (!hasAdvantage)
+            {
+                diceRoll = Math.ceil(Math.random() * 20);
+            }
+            else
+            {
+                diceRoll = Math.max(
+                    Math.ceil(Math.random() * 20),
+                    Math.ceil(Math.random() * 20));
+            }
+            return diceRoll + modifier + (modifier / 100);
         }
-        return
-        { Process : ProcessParticipantList };
-    }
 
+        function ProcessParticipantList(participants) {
+            var combatList = [];
+            console.log(participants);
+            for (var part in participants) {
+                console.log(participants[part]);
+                var score = 0;
+                if (!participants[part].roll) {
+                    score= participants[part].mod;
+                }
+                else {
+                    score = RollInit(participants[part].mod, participants[part].adv);
+                }
 
-)();
+                combatList.push(
+                    {
+                        'name': participants[part].name,
+                        'score': score,
+                        'status': ''
+                    }
+                );
+            }
+            console.log(combatList);
+            return combatList;
+        }
+        return {
+            Process : ProcessParticipantList
+        };
+    }());
