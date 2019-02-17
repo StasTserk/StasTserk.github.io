8cdc9b7353ced9f4d1c7f7cca47dd0c919f9cc2b
Stas Tserkovny <stas.tserkovny@gmail.com>
2019-02-16 18:13:52 -0700

    Add star wars dice calculator
    Initial implementation for star wars dice calculator

---
+++ starwars/starWars.js
@@ -0,0 +1,92 @@
+var starWarsController = angular.module('starWarsController', []);
+
+const yellow = [
+    new Result(0, 0, 0, 1/12),
+    new Result(1, 0, 0, 1/6),
+    new Result(2, 0, 0, 1/6),
+    new Result(0, 1, 0, 1/12),
+    new Result(1, 1, 0, 1/4),
+    new Result(0, 2, 0, 1/6),
+    new Result(1, 0, 1, 1/12)
+];
+
+const red = [
+    new Result(0, 0, 0, 1/12),
+    new Result(-1, 0, 0, 1/6),
+    new Result(-2, 0, 0, 1/6),
+    new Result(0, -1, 0, 1/6),
+    new Result(-1, -1, 0, 1/6),
+    new Result(0, -2, 0, 1/6),
+    new Result(-1, 0, -1, 1/12)
+];
+
+const green = [
+    new Result(0, 0, 0, .125),
+    new Result(1, 0, 0, .25),
+    new Result(2, 0, 0, .125),
+    new Result(0, 1, 0, .25),
+    new Result(1, 1, 0, .125),
+    new Result(0, 2, 0, .125),
+];
+
+const purple = [
+    new Result(0, 0, 0, .125),
+    new Result(-1, 0, 0, .125),
+    new Result(-2, 0, 0, .125),
+    new Result(0, -1, 0, .375),
+    new Result(-1, -1, 0, .125),
+    new Result(0, -2, 0, .125),
+];
+
+const blue = [
+    new Result(0, 0, 0, 1/3),
+    new Result(1, 0, 0, 1/6),
+    new Result(0, 1, 0, 1/6),
+    new Result(1, 1, 0, 1/6),
+    new Result(0, 2, 0, 1/6),
+];
+
+const black = [
+    new Result(0, 0, 0, 1/3),
+    new Result(-1, 0, 0, 1/3),
+    new Result(0, -1, 0, 1/3)
+];
+
+starWarsController.controller('StarWarsController', function($scope) {
+    $scope.yellow = 1;
+    $scope.purple = 2;
+    $scope.green = 2;
+    $scope.black = 0;
+    $scope.blue = 1;
+    $scope.red = 0;
+    $scope.trigger = 2;
+
+    $scope.success = 0;
+    $scope.abilityTriggered = 0;
+    $scope.results = [];
+
+    $scope.calculate = function() {
+        const resultRange = new ResultRange([new Result(0, 0, 0, 1.0)]);
+        const final = resultRange
+            .multiplyByDie(yellow, $scope.yellow)
+            .multiplyByDie(green, $scope.green)
+            .multiplyByDie(blue, $scope.blue)
+            .multiplyByDie(red, $scope.red)
+            .multiplyByDie(purple, $scope.purple)
+            .multiplyByDie(black, $scope.black);
+
+        let r;
+        $scope.success = 0;
+        $scope.abilityTriggered = 0;
+
+        for (r of final.results) {
+            if (r.success > 0) {
+                $scope.success += r.probability * 100;
+                if (r.advantage >= $scope.trigger || r.triumph) {
+                    $scope.abilityTriggered += r.probability * 100;
+                }
+            }
+        }
+        $scope.results = final.results;
+    }
+});
